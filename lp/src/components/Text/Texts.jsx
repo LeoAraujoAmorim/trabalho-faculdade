c40781148f2cd721c0d98978e647edb1017f5b2a
LeoAraujoAmorim <leoaamorim31@gmail.com>
2024-05-18 13:59:59 -0300

    tags removidas/correção de erro

--- lp/src/components/Text/Texts.jsx
+++ lp/src/components/Text/Texts.jsx
@@ -4,12 +4,12 @@ import Image from "next/image";
 export default function Text(props) {
     return (
         <div className="text-xl">
-            <p>
-                {props.text.map((text, index) => (
-                  <li className=' my-4' key={index}>
+
+            {props.text.map((text, index) => (
+                <li className=' my-4' key={index}>
                     {text}
-                  </li>))}
-            </p>
+                </li>))}
+
         </div>
     )
 }
--- lp/src/components/navbar/Navbar.jsx
+++ lp/src/components/navbar/Navbar.jsx
@@ -4,13 +4,13 @@ export default function Navbar() {
         <div className="navbar justify-center bg-blue-600 fixed  border-b border-b-black ">
             <div className="navbar-center lg:flex">
                 <ul className="menu menu-horizontal px-1 text-xl text-white playfair-display">
-                    <Link href="/#fashion"><li><a>Fashion</a></li></Link>
-                    <Link href="/#Systems Analysis"><li><a>Systems Analysis</a></li></Link>
-                    <Link href="/#Veterinary Medicine"><li><a>Veterinary Medicine</a></li></Link>
-                    <Link href="/#Graphic Design"><li><a>Graphic Design</a></li></Link>
-                    <Link href="/#Interior Design"><li><a>Interior Design</a></li></Link>
-                    <Link href="/#Music Production"><li><a>Music Production</a></li></Link>
-                    <Link href="/#Marketing"><li><a>Marketing</a></li></Link>
+                    <Link href="/#fashion"><li>Fashion</li></Link>
+                    <Link href="/#Systems Analysis"><li>Systems Analysis</li></Link>
+                    <Link href="/#Veterinary Medicine"><li>Veterinary Medicine</li></Link>
+                    <Link href="/#Graphic Design"><li>Graphic Design</li></Link>
+                    <Link href="/#Interior Design"><li>Interior Design</li></Link>
+                    <Link href="/#Music Production"><li>Music Production</li></Link>
+                    <Link href="/#Marketing"><li>Marketing</li></Link>
                 </ul>
             </div>
         </div>
